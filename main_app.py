bb4a49410268bdcbb59e057aab8876f79e7c4e40
Kravmi <144707588+Kravmi@users.noreply.github.com>
2024-01-22 17:46:46 +0300

    Initial commit

---
+++ main_app.py
@@ -0,0 +1,7 @@
+# напиши здесь свое приложение
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
