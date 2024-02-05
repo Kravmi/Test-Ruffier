0ef40a4a4ed604836efade96a96c0053db3348ef
Kravmi <144707588+Kravmi@users.noreply.github.com>
2024-02-05 16:59:56 +0300

    Начали создание анимации кнопки приседания.

--- runner.py
+++ runner.py
@@ -1,12 +1,30 @@
 # напиши модуль для работы с анимацией
+from kivy.animation import Animation
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, BooleanProperty
+from kivy.uix.boxlayout import BoxLayout
 
 class Runner(BoxLayout):
-
-    def __init__(self):
-        pass
+    finished = BooleanProperty(False)
+    value = NumericProperty(0)
+    def __init__(self, total=10, step_time=1):
+        self.total = total
+        self.step_time = step_time
+        self.btn = Button(text = 'Присядь!!!', size_hint = (1, 0.1), pos_hint = ('top': 1.0))
+        self.animation = (Animation(pos_hint={'top': 0.1}, duration=steptime/2)
+                       + Animation(pos_hint={'top': 1.0}, duration=steptime/2))
+        self.add_widget(self.btn)
+        super().__init__(**kwargs)
 
     def start(self):
-        pass
+        self.value = 0
+        self.finished = False
+        self.animation.repeat = True
+        self.animation.start()
 
     def next(self, widget, step):
-        pass
+        if step == 1.0:
+            self.value += 1
+            if self.value > self.total:
+                self.animation.repeat = False
+                self.finished = True
