332b58bcab2287394d183c0267368a95082c3258
Kravmi <144707588+Kravmi@users.noreply.github.com>
2024-02-09 17:07:47 +0300

    Проработали оформление приложения.

--- main_app.py
+++ main_app.py
@@ -10,7 +10,22 @@ from seconds import Seconds
 import ruffier as ruf
 from runner import Runner
 from sits import Sits
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex, rgba
+from kivy.graphics import Color, Rectangle
 
+
+print(rgba('#098203'))
+main_scr = rgba('#098203')
+main_btn = get_color_from_hex('#076402')
+first_scr = rgba('#0320e4')
+first_btn = get_color_from_hex('#041aab')
+second_scr = rgba('#8606df')
+second_btn = get_color_from_hex('#5e039e')
+third_scr = rgba('#04b87f')
+third_btn = get_color_from_hex('#057552')
+result_scr = rgba('#bf2b0a')
+result_btn = get_color_from_hex('#8f1e06')
 
 def check_int(str_num):
     try:
@@ -32,12 +47,17 @@ class MyApp(App):
 class MainScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        with self.canvas.before: 
+            Color(*main_scr)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
         v = BoxLayout(orientation = 'vertical')
         h = BoxLayout()
-        instruction = Label(text = instructions.txt_instruction)
-        self.text_name = TextInput(text = 'Введите имя')
-        self.age = TextInput(text = 'Введите возраст')
-        start = Button(text = 'начать')
+        instruction = Label(text = instructions.txt_instruction, size_hint = (0.8, 2))
+        self.text_name = TextInput(text = 'Введите ваше имя:')
+        self.age = TextInput(text = 'Введите свой возраст:')
+        start = Button(text = 'Начать')
+        start.background_color = main_btn
         start.on_press = self.next
         h.add_widget(self.text_name)
         h.add_widget(self.age)
@@ -45,6 +65,10 @@ class MainScr(Screen):
         v.add_widget(h)
         v.add_widget(start)
         self.add_widget(v)
+
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
 
     def next(self):
         global text_name, age
@@ -60,14 +84,19 @@ class MainScr(Screen):
 class FirstScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        with self.canvas.before: 
+            Color(*first_scr)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
         self.level = False
         self.lbl_sec = Seconds(15)
         self.lbl_sec.bind(done = self.sec_finished)
         v = BoxLayout(orientation = 'vertical')
         txt = Label(text = instructions.txt_test1)
-        self.text_input = TextInput(text = 'Введите свой пульс')
+        self.text_input = TextInput(text = 'Введите свой пульс:')
         self.text_input.set_disabled(True)
         self.btn = Button(text = 'Начать')
+        self.btn.background_color = first_btn
         self.btn.on_press = self.next
         v.add_widget(txt)
         v.add_widget(self.lbl_sec)
@@ -96,19 +125,29 @@ class FirstScr(Screen):
             else:
                 self.manager.current = 'scr2'
 
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
+
 
 class SecondScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        with self.canvas.before: 
+            Color(*second_scr)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
         self.bool = False
         self.run = Runner(total = 30, step_time = 1)
         self.run.bind(finished = self.run_finished)
+        self.run.btn.background_color = second_btn
         self.sit = Sits(30)
         v = BoxLayout(orientation = 'vertical')
         v2 = BoxLayout(orientation = 'vertical')
         h = BoxLayout()
         txt = Label(text = instructions.txt_test2)
         self.btn = Button(text = 'Начать')
+        self.btn.background_color = second_btn
         self.btn.on_press = self.next
         v2.add_widget(self.sit)
         v2.add_widget(self.run)
@@ -131,10 +170,18 @@ class SecondScr(Screen):
         else:
             self.manager.current = 'scr3'
 
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
+
 
 class ThirdScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        with self.canvas.before: 
+            Color(*third_scr)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
         self.bool = False
         self.level = 0 
         self.lbl_sec = Seconds(15)
@@ -142,11 +189,12 @@ class ThirdScr(Screen):
         v = BoxLayout(orientation = 'vertical')
         h = BoxLayout()
         self.txt = Label(text = instructions.txt_test3)
-        self.text_input1 = TextInput(text = 'Введите свой пульс после упражнений')
+        self.text_input1 = TextInput(text = 'Введите свой пульс после упражнений:')
         self.text_input1.set_disabled(True)
-        self.text_input2 = TextInput(text = 'Введите свой пульс после отдыха')
+        self.text_input2 = TextInput(text = 'Введите свой пульс после отдыха:')
         self.text_input2.set_disabled(True)
         self.btn = Button(text = 'Продолжить')
+        self.btn.background_color = third_btn
         self.btn.on_press = self.next
         h.add_widget(self.text_input1)
         h.add_widget(self.text_input2)
@@ -173,6 +221,10 @@ class ThirdScr(Screen):
                 self.text_input2.set_disabled(False)
                 self.bool = True
 
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
+
     def next(self):
         global puls2, puls3
         if self.bool == False:
@@ -193,15 +245,21 @@ class ThirdScr(Screen):
 class ResultScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        with self.canvas.before: 
+            Color(*result_scr)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.bind(size=self._update_rect, pos=self._update_rect)
         self.result = Label(text = "")
 
         self.on_enter = self.before
         self.add_widget(self.result)
 
+    def _update_rect(self, instance, value):
+        self.rect.size = instance.size
+        self.rect.pos = instance.pos
+
     def before(self):
         self.result.text = text_name + '\n' + ruf.test(puls1, puls2, puls3, age)
-
-
 
 
 app = MyApp()
--- sits.py
+++ sits.py
@@ -8,10 +8,10 @@ class Sits(Label):
     def __init__(self, total, **kwargs):
         self.total = total
         self.current = 0
-        text_ = f'Осталось приседаний {self.total}'
+        text_ = f'Осталось приседаний: {self.total}'
         super().__init__(text = text_, **kwargs)
 
     def next(self, *args):
         self.current += 1
         result = max(0, self.total - self.current)
-        self.text = 'Осталось приседаний' + str(result)
+        self.text = 'Осталось приседаний: ' + str(result)
